8e6f66628c46ee34b0e407de7c83e44b9f4e7b48
mohdarshnadeem <arsh.nadeem@tothenew.com>
2025-03-12 20:54:54 +0530

    Spring Data JPA Part2

---
+++ src/main/java/com/arsh/SpringDataJpaPart2/SpringDataJpaPart2Application.java
@@ -0,0 +1,13 @@
+package com.arsh.SpringDataJpaPart2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaPart2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaPart2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/arsh/SpringDataJpaPart2/SpringDataJpaPart2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arsh.SpringDataJpaPart2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaPart2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
